c351d2ea683869ee4f8531e1e0c932da3c8d4fe7
Jon Andoni Galdos <jonandonigv@gmail.com>
2024-04-23 01:22:09 +0200

    server

--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+const (
+	HOST = "localhost"
+	TYPE = "tcp"
+	PORT = "8080"
+)
+
+func handleRequest(conn net.Conn) {
+	// Inconming request
+	buffer := make([]byte, 1024)
+	_, err := conn.Read(buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Write data ti response
+	time := time.Now().Format(time.ANSIC)
+	responseStr := fmt.Sprintf("Your message is: %v. Recieved time: %v", string(buffer[:]), time)
+	conn.Write([]byte(responseStr))
+
+	conn.Close()
+
+}
 
 func main() {
-	fmt.Println("Hello tcp")
+	listen, err := net.Listen(TYPE, HOST+":"+PORT)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	// Close the listener
+	defer listen.Close()
+
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+
+		go handleRequest(conn)
+
+	}
 }

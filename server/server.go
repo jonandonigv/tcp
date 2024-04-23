97c3e02cf0641b1dd17a95d5931b92a4d19dfae1
Jon Andoni Galdos <jonandonigv@gmail.com>
2024-04-23 02:19:27 +0200

    server: Server listen and Server creation

--- server/server.go
+++ server/server.go
@@ -1,4 +1,41 @@
 package server
 
+import (
+	"log"
+	"net"
+	"os"
+	"sync"
+)
+
 type Server struct {
+	Address           string
+	Listener          net.Listener
+	QuitChannel       chan struct{}
+	RecieveBuffer     chan Message
+	SendBuffer        chan string
+	Wg                sync.WaitGroup
+	ActiveConnections map[net.Conn]struct{}
+	ActiveConnMux     sync.Mutex
+}
+
+func CreateTCPServer(addr string) *Server {
+	return &Server{
+		Address:           addr,
+		QuitChannel:       make(chan struct{}),
+		RecieveBuffer:     make(chan Message, 10),
+		SendBuffer:        make(chan string, 10),
+		ActiveConnections: make(map[net.Conn]struct{}),
+	}
+}
+
+func (s *Server) Listen() error {
+	listener, err := net.Listen("tcp", s.Address)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	defer listener.Close()
+	s.Listener = listener
+
+	return nil
 }
---
+++ server/types.go
@@ -0,0 +1,10 @@
+package server
+
+type HeaderMessage struct {
+	FromAddress string
+}
+
+type Message struct {
+	Header  HeaderMessage
+	Payload []byte
+}

7e0da1a8365c0b17ba775535c25d56f996b201df
Jon Andoni Galdos <jonandonigv@gmail.com>
2024-04-24 18:23:16 +0200

    header

--- server/types.go
+++ server/types.go
@@ -1,5 +1,14 @@
 package server
 
+type TCPHeader struct {
+	SourceAddress      string
+	DestinationAddress string
+	SequenceNumber     int32
+	AcknolegedNumber   int16
+	DataOffset         int32
+	//TODO: Add the rest of the header bits
+}
+
 type HeaderMessage struct {
 	FromAddress string
 	// TODO: Add all the TCP header
